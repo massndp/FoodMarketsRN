07b29555ea49f9fab40b82f2e6f68facc99d9180
massndp <bayu8989.88@gmail.com>
2021-09-25 12:50:06 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, StyleSheet, Text} from 'react-native';
+import SplashScreen from './pages/SplashScreen';
+
+const App = () => {
+  return <SplashScreen />;
+};
+
+export default App;
